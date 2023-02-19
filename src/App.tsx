81fdb546a5503eda8e35df88a563ceea4de87785
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-19 10:39:19 +0530

    Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import HeroImage from "./HeroImage";
+import styled from "@emotion/styled";
+import { Global, css } from "@emotion/react";
+import globalStyles from "./globalStyle";
+
+const AppContainer = styled.div`
+  background-color: whitesmoke;
+`;
 
 export default function App() {
   return (
-    <>
+    <AppContainer>
+      <Global styles={globalStyles} />
       <HeroImage />
-    </>
+    </AppContainer>
   );
 }
---
+++ src/globalStyle.tsx
@@ -0,0 +1,13 @@
+import { css } from "@emotion/react";
+const globalStyles = css`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default globalStyles;
