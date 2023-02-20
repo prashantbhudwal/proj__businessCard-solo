5bfc575805a03d624c1a45527b27f5ec881bf3d0
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-20 09:43:39 +0530

    Add icons and colors

--- src/HeroContent.tsx
+++ src/HeroContent.tsx
@@ -1,5 +1,8 @@
 import styled from "@emotion/styled";
-import Button from "./Button";
+import IconButton from "./IconButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const StyledHeroContent = styled.div`
   text-align: center;
@@ -25,20 +28,31 @@ const StyledHeroContent = styled.div`
     font-size: 0.8rem;
   }
 
-  .buttonCOntainer {
+  .buttonContainer {
     display: flex;
+    gap: 1em;
   }
 `;
 
 export default function HeroContent() {
+  const linkedinIcon = <FontAwesomeIcon icon={faLinkedin} />;
+  const emailIcon = <FontAwesomeIcon icon={faEnvelope} />;
+  const primary = `#5093E2`;
+  const gray = `#D1D5DB`;
+  const white = `#fff`;
+  const black = `#000`;
   return (
     <StyledHeroContent>
       <h1>Prashant Bhudwal</h1>
       <h2>Frontend Developer</h2>
       <p>ideabox.substack.com</p>
       <div className="buttonContainer">
-        <Button outlined>Email</Button>
-        <Button primary>LinkedIn</Button>
+        <IconButton color={black} backgroundColor={gray} icon={emailIcon}>
+          Email
+        </IconButton>
+        <IconButton color={white} backgroundColor={primary} icon={linkedinIcon}>
+          LinkedIn
+        </IconButton>
       </div>
     </StyledHeroContent>
   );
