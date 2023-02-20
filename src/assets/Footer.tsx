fbb8d2e07616bca120247b68da764a7fbbcfa098
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-20 10:53:11 +0530

    Add the footer component

--- src/assets/Footer.tsx
+++ src/assets/Footer.tsx
@@ -1,9 +1,28 @@
 import styled from "@emotion/styled";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faTwitter,
+  faHashnode,
+} from "@fortawesome/free-brands-svg-icons";
 
 const StyledFooter = styled.div`
-  background-color: black;
+  display: flex;
+  justify-content: center;
+  gap: 1em;
+  color: white;
+  background: #161619;
+  border-radius: 0px 0px 10px 10px;
+  font-size: 1.75rem;
+  padding: 0.5em 1em;
 `;
 
 export default function Footer() {
-  return <StyledFooter>Testing</StyledFooter>;
+  return (
+    <StyledFooter>
+      <FontAwesomeIcon icon={faGithub} />
+      <FontAwesomeIcon icon={faTwitter} />
+      <FontAwesomeIcon icon={faHashnode} />
+    </StyledFooter>
+  );
 }
